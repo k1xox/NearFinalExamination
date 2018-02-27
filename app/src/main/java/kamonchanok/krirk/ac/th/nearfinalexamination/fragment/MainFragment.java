adacb2c8612891bf73f005c8724f86a49b639d66
kamonchanok <primkamonchanok@gmail.com>
2018-02-27 10:11:07 +0700

    2

---
+++ app/src/main/java/kamonchanok/krirk/ac/th/nearfinalexamination/fragment/MainFragment.java
@@ -0,0 +1,24 @@
+package kamonchanok.krirk.ac.th.nearfinalexamination.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import kamonchanok.krirk.ac.th.nearfinalexamination.R;
+
+/**
+ * Created by User on 27/2/2561.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        return view;
+    }
+}
