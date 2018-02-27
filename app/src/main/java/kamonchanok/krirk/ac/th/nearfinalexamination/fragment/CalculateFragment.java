85f301ad757eef57f1ed11a7fdab2bd4cd63d472
kamonchanok <primkamonchanok@gmail.com>
2018-02-27 11:04:06 +0700

    Back Controller

--- app/src/main/java/kamonchanok/krirk/ac/th/nearfinalexamination/fragment/CalculateFragment.java
+++ app/src/main/java/kamonchanok/krirk/ac/th/nearfinalexamination/fragment/CalculateFragment.java
@@ -3,42 +3,49 @@ package kamonchanok.krirk.ac.th.nearfinalexamination.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
+import kamonchanok.krirk.ac.th.nearfinalexamination.MainActivity;
 import kamonchanok.krirk.ac.th.nearfinalexamination.R;
 
 /**
  * Created by User on 27/2/2561.
  */
 
-public class MainFragment extends Fragment{
-
+public class CalculateFragment extends Fragment{
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-//        Calculate Controller
-        Button button = getView().findViewById(R.id.btnCalculate);
-        button.setOnClickListener(new View.OnClickListener() {
+//Creat Toolbar
+
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                getActivity().getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.contentMainFragment,new CalculateFragment())
-                        .addToBackStack(null)
-                        .commit();
+                getActivity().getSupportFragmentManager().popBackStack();
             }
         });
-    }
+
+//        Exchange Cntroller
+
+
+
+    }//Main Method
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_main,container,false);
+        View view = inflater.inflate(R.layout.fragment_calculate,container,false);
         return view;
     }
 }
